44d38e890887bcdb9d9c8ac19331e74d677ddfb5
Aejasbasha <43529647+Aejasbasha@users.noreply.github.com>
2018-12-19 09:54:14 +0530

    Update and rename count number to count number.c

--- count number.c
+++ count number.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    long long num;
+    long long n;
     int count = 0;
 
-    scanf("%lld", &num);
-    while(num != 0)
+    scanf("%lld", &n);
+    while(n != 0)
     {
         count++;
-        num /= 10;
+        n /= 10;
     }
     printf("%d", count);
     return 0;
